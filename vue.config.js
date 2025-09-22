486e3a144d5030e76e45d205dd1b2e3c3ff56c18
Gabriel Alves <rebelo.alves@hotmail.com>
2025-09-22 16:28:56 -0400

    alterações para funcionamento do deploy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/calcutalor-vue/'
-    : '/'
+    : '/',
+  outputDir: 'dist',
+  assetsDir: 'static'
 }
